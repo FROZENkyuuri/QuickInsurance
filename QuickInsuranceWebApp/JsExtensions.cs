1f9a2496f9dbfd90cac982b236ab550e1cfa672b
koike <hdkkik@gmail.com>
2021-08-28 04:27:00 +0900

    make jpyc value

---
+++ QuickInsuranceWebApp/JsExtensions.cs
@@ -0,0 +1,10 @@
+﻿public static class JsExtensions
+{
+    public static string LocalStorage = "blazorExtensions.";
+}
+
+public static class KeyList
+{
+    public static string Jpyc = "jpyc";
+    
+}
--- QuickInsuranceWebApp/LocalStorage.cs
+++ QuickInsuranceWebApp/LocalStorage.cs
@@ -1,9 +1,8 @@
 // https://www.idnet.co.jp/column/page_160.html
-using System;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 
-public class LocalStorage
+public sealed class LocalStorage
 {
     IJSRuntime _js;
 
@@ -14,15 +13,15 @@ public class LocalStorage
 
     public async Task SetItem(string key, string value)
     {
-        await _js.InvokeVoidAsync("blazorExtensions.SetItem", key, value);
+        await _js.InvokeVoidAsync($"{JsExtensions.LocalStorage}SetItem", key, value);
     }
 
     public async Task<string> GetItem(string key)
     {
-        return await _js.InvokeAsync<string>("blazorExtensions.GetItem", key);
+        return await _js.InvokeAsync<string>($"{JsExtensions.LocalStorage}GetItem", key);
     }
 
     public async Task RemoveItem(string key){
-        await _js.InvokeVoidAsync("blazorExtensions.RemoveItem", key);
+        await _js.InvokeVoidAsync($"{JsExtensions.LocalStorage}RemoveItem", key);
     }
 }
